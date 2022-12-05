583264ff7ac1abbb2522fd2aa8066a9918341f94
OlegAnisimov <gramafon@yandex.ru>
2022-12-05 11:58:03 +0300

    get-news-list.service.ts

---
+++ src/app/services/get-news-list/get-news-list.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { NewsResponse } from "../../types";
+import { takeUntil } from "rxjs/operators";
+import { UnsubscribeService } from "../utility/unsubscribe-service/unsubscribe.service";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetNewsListService {
+  protected ngUnsubscribe$: UnsubscribeService;
+  testNumberOfNews: number;
+
+
+  constructor(
+    private http: HttpClient,
+    protected injector: Injector,) {
+    this.ngUnsubscribe$ = injector.get(UnsubscribeService);
+  }
+
+  getParametersFrontPage(testUrl: string,
+  ) {
+    return this.http.get<NewsResponse>(testUrl)
+      .pipe(takeUntil(this.ngUnsubscribe$));
+  }
+
+  getAllFrontNews(url: string, numberOfNews: number, urlFrontPage: string) {
+    let modifyUrl: string = `${url}${numberOfNews}${urlFrontPage}`;
+    return this.http.get<NewsResponse>(modifyUrl)
+      .pipe(takeUntil(this.ngUnsubscribe$));
+  }
+}
