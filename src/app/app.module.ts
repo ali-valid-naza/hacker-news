ae01412f70b86d36c5ab89293936550196661054
OlegAnisimov <gramafon@yandex.ru>
2022-12-05 11:29:55 +0300

    material.module.ts add imports to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    HttpClientModule,
+    MaterialModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
